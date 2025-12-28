d50581e117400e69926cde0a757a7b2e7354d9dc
dev.justin <justinpaul1025@gmail.com>
2025-12-28 22:59:02 +0800

    update

--- database/migrations/2025_12_28_133554_remove_microchip_number_from_patients_table.php
+++ database/migrations/2025_12_28_133554_remove_microchip_number_from_patients_table.php
@@ -11,9 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('patients', function (Blueprint $table) {
-            $table->dropColumn('microchip_number');
-        });
+        if (Schema::hasColumn('patients', 'microchip_number')) {
+            Schema::table('patients', function (Blueprint $table) {
+                $table->dropColumn('microchip_number');
+            });
+        }
     }
 
     /**
@@ -21,8 +23,10 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('patients', function (Blueprint $table) {
-            $table->string('microchip_number', 100)->nullable();
-        });
+        if (!Schema::hasColumn('patients', 'microchip_number')) {
+            Schema::table('patients', function (Blueprint $table) {
+                $table->string('microchip_number', 100)->nullable();
+            });
+        }
     }
 };
